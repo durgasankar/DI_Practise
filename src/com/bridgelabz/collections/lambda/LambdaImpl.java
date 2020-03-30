59ca641cbf1f720f5d012fe2e716fe4443d49b33
durgasankar <durgasankar.raja500@gmail.com>
2020-03-30 18:36:41 +0530

    [durgasankar].add -> impl of funcational interface example prime checker  using lambda java 8

--- src/com/bridgelabz/collections/lambda/LambdaImpl.java
+++ src/com/bridgelabz/collections/lambda/LambdaImpl.java
@@ -21,9 +21,37 @@ public class LambdaImpl {
         return checker;
     }
 
+    PrimeChecker primeChecker(){
+        PrimeChecker checker = number -> {
+            if (number <= 0) {
+                return false;
+            } else if (number == 1) {
+                return false;
+            } else {
+                for(int divisor = 2; divisor <= number / 2; ++divisor) {
+                    if (number % divisor == 0) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        };
+        return checker;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Enter a number : ");
+        /*takes impl class of functional interface and number from user and checks whether even or odd
+        then prints the status*/
+
+        System.out.println("Enter a number for even odd checker : ");
         System.out.println(( checker( new LambdaImpl().checker(), Util.scanner.nextInt()) == 0 ) ? "Even" : "Odd" );
+
+        /*Prime checker using lambda functional interface.*/
+
+        System.out.println("Enter a number for prime checker : ");
+        System.out.println(new LambdaImpl().primeChecker().isPrime(Util.scanner.nextInt()) ? "Prime Number" : "Not Prime");
+
+
 
     }
 }
---
+++ src/com/bridgelabz/collections/lambda/PrimeChecker.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.collections.lambda;
+
+/**
+ * Marker Interface which has only one method.
+ *
+ * @author Durgasankar Mishra
+ * @version 1.8
+ * @created 2020-03-30
+ */
+
+@FunctionalInterface
+public interface PrimeChecker {
+    boolean isPrime(int number);
+}
