04a531d459c643a8150bc7085c12fcdfe8d0597b
durgasankar <durgasankar.raja500@gmail.com>
2020-03-31 14:08:24 +0530

    "[durgasankar].add ->  iterate ArrayList using for-loop, while-loop, and advance for-loop.  "

--- src/com/bridgelabz/corejava/BasicQuestions.java
+++ src/com/bridgelabz/corejava/BasicQuestions.java
@@ -1,5 +1,10 @@
 package com.bridgelabz.corejava;
 
+import com.bridgelabz.collections.predicatelambda.Person;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class BasicQuestions {
 
     private void countVowelsConsonantsWhiteSpacesInString(String inputString) {
@@ -18,11 +23,46 @@ public class BasicQuestions {
                 + consonants + ", Blank spaces : " + whiteSpaces);
     }
 
+    private void iterateUsingForLoop(List<Person> personList) {
+        List<String> forLoopPersonList = new ArrayList<>(personList.size());
+        List<String> advancedLoopPersonList = new ArrayList<>(personList.size());
+        List<String> whileLoopPersonList = new ArrayList<>(personList.size());
+
+        System.out.println("Adding all names to for loop...");
+        for (int index = 0; index < personList.size(); index++) {
+            forLoopPersonList.add(personList.get(index).getName());
+        }
+        System.out.println("Adding all names to advanced for loop...");
+        for (Person fetchedPerson : personList) {
+            advancedLoopPersonList.add(fetchedPerson.getName());
+        }
+        System.out.println("Adding all names to while loop...");
+        int counter = 0;
+        while (personList.size() > counter) {
+            whileLoopPersonList.add(personList.get(counter).getName());
+            counter++;
+        }
+        System.out.println("Printing all names : ");
+        System.out.println("For loop all names : " + forLoopPersonList.toString());
+        System.out.println("Advanced for loop all names : " + advancedLoopPersonList.toString());
+        System.out.println("While loop all names : " + whileLoopPersonList.toString());
+    }
+
 
     public static void main(String[] args) {
         BasicQuestions questions = new BasicQuestions();
         System.out.println("count vowels and consonants in String -> ");
         questions.countVowelsConsonantsWhiteSpacesInString("Hy this is counting practise lets test.");
+
+        System.out.println("Iterate ArrayList using for-loop, while-loop, and advance for-loop : ");
+        List<Person> personList = Arrays.asList(new Person(10, "Ramesh", "IN", "Karnataka"),
+                new Person(30, "Durgasankar", "IN", "Goa"),
+                new Person(20, "John", "USA", "New York"),
+                new Person(55, "Mic", "USA", "Chicago"),
+                new Person(20, "Stanley", "USA", "Alaska"),
+                new Person(50, "Jack", "AUS", "Sydney"),
+                new Person(40, "Chang", "CHI", "Wuhan"));
+        questions.iterateUsingForLoop(personList);
 
     }
 }
