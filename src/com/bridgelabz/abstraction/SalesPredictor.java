08e4634e5d8a0bcac6345157bd13800c9666b949
durgasankar <durgasankar.raja500@gmail.com>
2020-04-10 14:23:51 +0530

    [durgasankar].update -> refactor duplicate codes on Use of Interface demo example using java 8

--- src/com/bridgelabz/abstraction/SalesPredictor.java
+++ src/com/bridgelabz/abstraction/SalesPredictor.java
@@ -32,30 +32,19 @@ public class SalesPredictor {
                 new ClientEngagement( "Jim", 25, 60000 )
         );
 //        fixed rate
-        System.out.println("fixed rate : ");
-        RevenueCalculator calculator = new FixedFeeCalculator( FixedFeeCalculator.STANDARD_FEE );
-        //        calculate revenue
-
-        double total = printTotalRevenue( engagements, calculator );
-        System.out.println( "total = " + total );
-
-        double advancedForLoopTotal = getAdvancedForLoopTotal( engagements, calculator );
-        System.out.println( "advancedForLoopTotal = " + advancedForLoopTotal );
-
-//        Royalty percentage rate
-        System.out.println("royalty percentage rate : ");
-        calculator = new RoyaltyShareCalculator( RoyaltyShareCalculator.STANDARD_ROYALTY_PERCENTAGE );
-        //        calculate revenue
-        System.out.println( "total = " +  printTotalRevenue( engagements, calculator ) );
-
-//        Hourly rate
-        System.out.println("hourly rate rate : ");
-        calculator = new HourlyRateRevenueCalculator( HourlyRateRevenueCalculator.STANDARD_HOURLY_RATE );
+        System.out.println( "fixed rate : " );
 //        calculate revenue
-        System.out.println( "total = " +  printTotalRevenue( engagements, calculator ) );
+        System.out.println( "total = " + printTotalRevenue( engagements, new FixedFeeCalculator( FixedFeeCalculator.STANDARD_FEE ) ) );
+        System.out.println( "advancedForLoopTotal = " + getAdvancedForLoopTotal( engagements, new FixedFeeCalculator( FixedFeeCalculator.STANDARD_FEE ) ) );
+//        Royalty percentage rate
+        System.out.println( "royalty percentage rate : " );
+        System.out.println( "total = " + printTotalRevenue( engagements, new RoyaltyShareCalculator( RoyaltyShareCalculator.STANDARD_ROYALTY_PERCENTAGE ) ) );
+//        Hourly rate
+        System.out.println( "hourly rate rate : " );
+//        calculate revenue
+        System.out.println( "total = " + printTotalRevenue( engagements, new HourlyRateRevenueCalculator( HourlyRateRevenueCalculator.STANDARD_HOURLY_RATE ) ) );
 
     }
-
 
 
 }
